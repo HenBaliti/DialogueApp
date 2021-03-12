bab44d548f91875a7492d1a6b9ea307485b9a528
Amit Shabo <amitshabo123@gmail.com>
2021-03-12 21:36:20 +0200

    add register fragment and btn

--- app/src/main/java/com/example/dialogueapp/fragment_login.java
+++ app/src/main/java/com/example/dialogueapp/fragment_login.java
@@ -17,7 +17,13 @@ public class fragment_login extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
-
+        Button signupBtn = view.findViewById(R.id.button_signup);
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_fragment_login_to_fragment_register);
+            }
+        });
         return view;
     }
 }
