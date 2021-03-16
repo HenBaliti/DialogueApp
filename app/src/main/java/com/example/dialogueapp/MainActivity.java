6289b68f6cec46164654bcee8a1eb03b786e9d03
Hen Baliti <58342958+HenBaliti@users.noreply.github.com>
2021-03-17 01:45:39 +0200

    Building the first page -> Schedule lesson for students , on picking date and hour when you click "find" it will show you a FlatList of the free teachers right at the same date and hour.

--- app/src/main/java/com/example/dialogueapp/MainActivity.java
+++ app/src/main/java/com/example/dialogueapp/MainActivity.java
@@ -1,26 +1,83 @@
 package com.example.dialogueapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavController navController;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+
+        //Bottom Navigation Controller
         NavigationUI.setupWithNavController(bottomNav,navController);
+
+//        //Drawer Navigation Controller
+        mToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                Intent intent;
+//                if(item.getItemId()==R.id.fragment_home)
+//                    Log.d("MsgAlert","This is home clicking");
+//                switch (item.getItemId()) {
+//                    case R.id.fragment_home :
+//                        Log.d("Checking","--------------------------------------------------------");
+//                        intent = new Intent(MainActivity.this,fragment_home.class);
+//                        break;
+//                    case R.id.fragment_schedule_student :
+//                        intent = new Intent(MainActivity.this,fragment_home.class);
+//                        break;
+//                    case R.id.fragment_history :
+//                        intent = new Intent(MainActivity.this,fragment_home.class);
+//                        break;
+//                }
+//                return true;
+//            }
+//        });
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 
 
--- app/src/main/java/com/example/dialogueapp/fragment_schedule_student.java
+++ app/src/main/java/com/example/dialogueapp/fragment_schedule_student.java
@@ -1,5 +1,7 @@
 package com.example.dialogueapp;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +9,96 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment_schedule_student#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Calendar;
+
+
 public class fragment_schedule_student extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public fragment_schedule_student() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_schedule_student.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_schedule_student newInstance(String param1, String param2) {
-        fragment_schedule_student fragment = new fragment_schedule_student();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule_student, container, false);
+        View view =  inflater.inflate(R.layout.fragment_schedule_student, container, false);
+
+        ImageButton clickScheduleBtn = (ImageButton)view.findViewById(R.id.btn_schedule_picker);
+
+
+        // Using an onclick listener on the editText to show the datePicker
+        clickScheduleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerFragment dpf = new DatePickerFragment().newInstance();
+                dpf.setCallBack(onDate);
+                dpf.show(getFragmentManager().beginTransaction(), "DatePickerFragment");
+            }
+        });
+
+
+        //----------------------------------------------------
+        // Get open TimePickerDialog button.
+        ImageButton timePickerDialogButton = (ImageButton)view.findViewById(R.id.btn_time_picker);
+        timePickerDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new OnTimeSetListener instance. This listener will be invoked when user click ok button in TimePickerDialog.
+                TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                        StringBuffer strBuf = new StringBuffer();
+//                        strBuf.append("You select time is ");
+                        strBuf.append(String.valueOf(hour));
+                        strBuf.append(":");
+                        strBuf.append(String.valueOf(minute));
+//                        Log.d("Tagggg",strBuf.toString());
+
+                        TextView timePickerValueTextView = (TextView)getActivity().findViewById(R.id.txt_picked_time);
+                        timePickerValueTextView.setText(strBuf.toString());
+                    }
+                };
+
+                Calendar now = Calendar.getInstance();
+                int hour = now.get(java.util.Calendar.HOUR_OF_DAY);
+                int minute = now.get(java.util.Calendar.MINUTE);
+
+                // Whether show time in 24 hour format or not.
+                boolean is24Hour = true;
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), onTimeSetListener, hour, minute, is24Hour);
+
+                timePickerDialog.setIcon(R.drawable.ic_history);
+                timePickerDialog.setTitle("Please select time.");
+
+                timePickerDialog.show();
+            }
+        });
+
+
+
+        return view;
     }
+
+    DatePickerDialog.OnDateSetListener onDate = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+
+            TextView tvDate = (TextView) getActivity().findViewById(R.id.txt_picked_date);
+            tvDate.setText(String.valueOf(year) + "-" + String.valueOf(monthOfYear+1)
+                    + "-" + String.valueOf(dayOfMonth));
+        }
+    };
+
+
 }
