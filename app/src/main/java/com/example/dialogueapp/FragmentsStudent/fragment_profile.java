de24fea193fa8ebe9c09ba3790197e7d30f6f69b
Amit Shabo <51865606+amitshabo@users.noreply.github.com>
2021-04-06 13:35:12 +0300

    profile fragment for both teacher and student, only design and user details

--- app/src/main/java/com/example/dialogueapp/FragmentsStudent/fragment_profile.java
+++ app/src/main/java/com/example/dialogueapp/FragmentsStudent/fragment_profile.java
@@ -2,21 +2,95 @@ package com.example.dialogueapp.FragmentsStudent;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.dialogueapp.Model.Model;
+import com.example.dialogueapp.Model.User;
 import com.example.dialogueapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class fragment_profile extends Fragment {
-
+    private FirebaseAuth mAuth;
+    private TextView txtUserName, txtFullName, txtEmail, txtUserType, txtTopUserName, txtTopEmail;
+    private CircleImageView imageProfile;
+    private String UserID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+        TextView moveHome = view.findViewById(R.id.nav_profiletohome);
+        moveHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_fragment_profile_to_fragment_home);
+            }
+        });
+        txtUserName = view.findViewById(R.id.textUserName);
+        txtFullName = view.findViewById(R.id.textFullName);
+        txtEmail = view.findViewById(R.id.textEmail);
+        txtUserType = view.findViewById(R.id.textUserType);
+        imageProfile = view.findViewById(R.id.image_profile);
+        txtTopUserName = view.findViewById(R.id.id_usernametop);
+        txtTopEmail = view.findViewById(R.id.id_useremail);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        ImageButton logOutBtn = view.findViewById(R.id.btn_logout_profile);
+        if(user != null) {
+            // User is signed in
+            logOutBtn.setVisibility(view.VISIBLE);
+
+            //BUTTON LOGOUT -- Press
+            logOutBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    LogOutFunction();
+                    getActivity().finish();
+                }
+
+                private void LogOutFunction() {
+                    FirebaseAuth.getInstance().signOut();
+                }
+            });
+        } else {
+            // No user is signed in
+            logOutBtn.setVisibility(view.GONE);
+        }
+
+//
+//
+
+        Model.instance.GetUserObjByID(user.getEmail(), new Model.GetUserObjByEmailListener() {
+            @Override
+            public void onComplete(User currentUser) {
+                txtUserName.setText(currentUser.getUser_name());
+                txtFullName.setText(currentUser.getFull_name());
+                txtEmail.setText(currentUser.getEmail());
+                txtUserType.setText(currentUser.getUser_type());
+                txtTopUserName.setText(currentUser.getUser_name());
+                txtTopEmail.setText(currentUser.getEmail());
+                if (currentUser.getImageUrl() != null) {
+                    Log.d("check",currentUser.getImageUrl());
+                    Picasso.get().load(currentUser.getImageUrl()).into(imageProfile);
+
+                }
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/dialogueapp/FragmentsTeacher/fragment_profile_teacher.java
+++ app/src/main/java/com/example/dialogueapp/FragmentsTeacher/fragment_profile_teacher.java
@@ -3,64 +3,95 @@ package com.example.dialogueapp.FragmentsTeacher;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.example.dialogueapp.Model.Model;
+import com.example.dialogueapp.Model.User;
 import com.example.dialogueapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment_profile_teacher#newInstance} factory method to
- * create an instance of this fragment.
- */
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
 public class fragment_profile_teacher extends Fragment {
+    private FirebaseAuth mAuth;
+    private TextView txtUserName, txtFullName, txtEmail, txtUserType, txtTopUserName, txtTopEmail;
+    private CircleImageView imageProfile;
+    private String UserID;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public fragment_profile_teacher() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_profile_teacher.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_profile_teacher newInstance(String param1, String param2) {
-        fragment_profile_teacher fragment = new fragment_profile_teacher();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_teacher, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+        TextView moveHome = view.findViewById(R.id.nav_profiletohome);
+        moveHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_fragment_profile_to_fragment_home);
+            }
+        });
+        txtUserName = view.findViewById(R.id.textUserName);
+        txtFullName = view.findViewById(R.id.textFullName);
+        txtEmail = view.findViewById(R.id.textEmail);
+        txtUserType = view.findViewById(R.id.textUserType);
+        imageProfile = view.findViewById(R.id.image_profile);
+        txtTopUserName = view.findViewById(R.id.id_usernametop);
+        txtTopEmail = view.findViewById(R.id.id_useremail);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        ImageButton logOutBtn = view.findViewById(R.id.btn_logout_profile);
+        if(user != null) {
+            // User is signed in
+            logOutBtn.setVisibility(view.VISIBLE);
+
+            //BUTTON LOGOUT -- Press
+            logOutBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    LogOutFunction();
+                    getActivity().finish();
+                }
+
+                private void LogOutFunction() {
+                    FirebaseAuth.getInstance().signOut();
+                }
+            });
+        } else {
+            // No user is signed in
+            logOutBtn.setVisibility(view.GONE);
+        }
+
+//
+//
+
+        Model.instance.GetUserObjByID(user.getEmail(), new Model.GetUserObjByEmailListener() {
+            @Override
+            public void onComplete(User currentUser) {
+                txtUserName.setText(currentUser.getUser_name());
+                txtFullName.setText(currentUser.getFull_name());
+                txtEmail.setText(currentUser.getEmail());
+                txtUserType.setText(currentUser.getUser_type());
+                txtTopUserName.setText(currentUser.getUser_name());
+                txtTopEmail.setText(currentUser.getEmail());
+                if (currentUser.getImageUrl() != null) {
+                    Log.d("check",currentUser.getImageUrl());
+                    Picasso.get().load(currentUser.getImageUrl()).into(imageProfile);
+
+                }
+            }
+        });
+        return view;
     }
+
 }
